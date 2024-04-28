31ef26e3777cdf509cf3c8e7bc4755c9fc82413f
ameersaria <164121817+ameersaria@users.noreply.github.com>
2024-04-28 12:26:25 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,162 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bold.yellowBright("\n\n\t============ Welcome to Ameer Saria's Adventure_Game ============"));
+// Classes Player & Opponent
+class Player {
+    name;
+    fuel = 100;
+    constructor(name) {
+        this.name = name;
+    }
+    fuelDecrease() {
+        let fuel = this.fuel - 25;
+        this.fuel = fuel;
+    }
+    fuelIncrease() {
+        this.fuel = 100;
+    }
+}
+class Opponent {
+    name;
+    fuel = 100;
+    constructor(name) {
+        this.name = name;
+    }
+    fuelDecrease() {
+        let fuel = this.fuel - 25;
+        this.fuel = fuel;
+    }
+}
+// Player name
+let player = await inquirer.prompt([{
+        name: "name",
+        type: "input",
+        message: "Please enter your name:"
+    }]);
+//   Select Opponent 
+let opponent = await inquirer.prompt([{
+        name: "select",
+        type: "list",
+        message: "Select your opponent",
+        choices: ["Skeleton", "Assasin", "Zombie"]
+    }]);
+// Gathered Data
+let p1 = new Player(player.name);
+let o1 = new Opponent(opponent.select);
+do {
+    // Skeleton  
+    if (opponent.select == "Skeleton") {
+        let ask = await inquirer.prompt([{
+                name: "opt",
+                type: "list",
+                message: "Select your opponent",
+                choices: ["Attack", "Drink Portion", "Run For Your Life"]
+            }]);
+        if (ask.opt == "Attack") {
+            let num = Math.floor(Math.random() * 2);
+            if (num > 0) {
+                p1.fuelDecrease();
+                console.log(chalk.bold.red(`${p1.name} fuel is ${p1.fuel}`));
+                console.log(chalk.bold.green(`${o1.name} fuel is ${o1.fuel}`));
+                if (p1.fuel <= 0) {
+                    console.log(chalk.red.bold.italic("You Loose, Better Luck Next Time"));
+                    process.exit();
+                }
+            }
+            if (num <= 0) {
+                o1.fuelDecrease();
+                console.log(chalk.bold.green(`${p1.name} fuel is ${p1.fuel}`));
+                console.log(chalk.bold.red(`${o1.name} fuel is ${o1.fuel}`));
+                if (o1.fuel <= 0) {
+                    console.log(chalk.green.bold.italic("You Win"));
+                    process.exit();
+                }
+            }
+        }
+        if (ask.opt == "Drink Portion") {
+            p1.fuelIncrease();
+            console.log(chalk.bold.italic.green(`You Drink Health Portion.Your fuel is ${p1.fuel}`));
+        }
+        if (ask.opt == "Run For Your Life") {
+            console.log(chalk.red.bold.italic("You Loose, Better Luck Next Time"));
+            process.exit();
+        }
+    }
+    // Assasin
+    if (opponent.select == "Assasin") {
+        let ask = await inquirer.prompt([{
+                name: "opt",
+                type: "list",
+                message: "Select your opponent",
+                choices: ["Attack", "Drink Portion", "Run For Your Life"]
+            }]);
+        if (ask.opt == "Attack") {
+            let num = Math.floor(Math.random() * 2);
+            if (num > 0) {
+                p1.fuelDecrease();
+                console.log(chalk.bold.red(`${p1.name} fuel is ${p1.fuel}`));
+                console.log(chalk.bold.green(`${o1.name} fuel is ${o1.fuel}`));
+                if (p1.fuel <= 0) {
+                    console.log(chalk.red.bold.italic("You Loose, Better Luck Next Time"));
+                    process.exit();
+                }
+            }
+            if (num <= 0) {
+                o1.fuelDecrease();
+                console.log(chalk.bold.green(`${p1.name} fuel is ${p1.fuel}`));
+                console.log(chalk.bold.red(`${o1.name} fuel is ${o1.fuel}`));
+                if (o1.fuel <= 0) {
+                    console.log(chalk.green.bold.italic("You Win"));
+                    process.exit();
+                }
+            }
+        }
+        if (ask.opt == "Drink Portion") {
+            p1.fuelIncrease();
+            console.log(chalk.bold.italic.green(`You Drink Health Portion.Your fuel is ${p1.fuel}`));
+        }
+        if (ask.opt == "Run For Your Life") {
+            console.log(chalk.red.bold.italic("You Loose, Better Luck Next Time"));
+            process.exit();
+        }
+    }
+    // Zoombie
+    if (opponent.select == "Zombie") {
+        let ask = await inquirer.prompt([{
+                name: "opt",
+                type: "list",
+                message: "Select your opponent",
+                choices: ["Attack", "Drink Portion", "Run For Your Life"]
+            }]);
+        if (ask.opt == "Attack") {
+            let num = Math.floor(Math.random() * 2);
+            if (num > 0) {
+                p1.fuelDecrease();
+                console.log(chalk.bold.red(`${p1.name} fuel is ${p1.fuel}`));
+                console.log(chalk.bold.green(`${o1.name} fuel is ${o1.fuel}`));
+                if (p1.fuel <= 0) {
+                    console.log(chalk.red.bold.italic("You Loose, Better Luck Next Time"));
+                    process.exit();
+                }
+            }
+            if (num <= 0) {
+                o1.fuelDecrease();
+                console.log(chalk.bold.green(`${p1.name} fuel is ${p1.fuel}`));
+                console.log(chalk.bold.red(`${o1.name} fuel is ${o1.fuel}`));
+                if (o1.fuel <= 0) {
+                    console.log(chalk.green.bold.italic("You Win"));
+                    process.exit();
+                }
+            }
+        }
+        if (ask.opt == "Drink Portion") {
+            p1.fuelIncrease();
+            console.log(chalk.bold.italic.green(`You Drink Health Portion.Your fuel is ${p1.fuel}`));
+        }
+        if (ask.opt == "Run For Your Life") {
+            console.log(chalk.red.bold.italic("You Loose, Better Luck Next Time"));
+            process.exit();
+        }
+    }
+} while (true);
